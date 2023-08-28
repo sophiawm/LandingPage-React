aa81c4fd76a1b2d6b8fe4422d9777b963a8b9265
Sophia WM <claudiasophiawong@gmail.com>
2023-08-28 22:24:00 +0200

    update general 2

---
+++ LandingPage-React/src/components/calltoaction.jsx
@@ -0,0 +1,14 @@
+function CallToAction () {
+    return (
+    <div className="call-to-container">
+        <img src="../images/central-icons.png" alt="Illustration for Call To Action" />
+        <div className="call-to-text-container">
+            <h1>Consultores teconológicos</h1>
+            <h4>Especialitas en desarrollo</h4>
+            <button>Te llamamos </button>
+        </div>
+    </div>
+    );
+}
+
+export default CallToAction
--- LandingPage-React/src/components/header.jsx
+++ LandingPage-React/src/components/header.jsx
@@ -1,14 +1,13 @@
 function Header () {
     return (
-        <>
-        <div className="header-container"></div>
+        <div className="header-container">
             <h1 className="logo-wehelp">WeHelp<span className="logo-you">You</span></h1>
             <ul className="menu-items">
                 <li>Somos</li>
                 <li>Equipo</li>
                 <li className="contacto">Contáctemos</li>
             </ul>
-        </>
+        </div>
     );
 }
 
