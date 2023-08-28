0ad3b5202e50ddc542b46c01fa30e385111c6a10
Sophia WM <claudiasophiawong@gmail.com>
2023-08-28 21:53:06 +0200

    update general

---
+++ LandingPage-React/src/components/header.jsx
@@ -0,0 +1,15 @@
+function Header () {
+    return (
+        <>
+        <div className="header-container"></div>
+            <h1 className="logo-wehelp">WeHelp<span className="logo-you">You</span></h1>
+            <ul className="menu-items">
+                <li>Somos</li>
+                <li>Equipo</li>
+                <li className="contacto">Contáctemos</li>
+            </ul>
+        </>
+    );
+}
+
+export default Header
